06f2e6b0dca021af6deceb61fa77f8ac67f004a3
Mohit Panjwani <mohit.panjvani@gmail.com>
2017-01-24 19:18:33 +0530

    .gitignore is now working

--- resources/views/admin/layouts/partials/header-bottom.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="header-bottom">
-    {{--@include('admin.layouts.partials.sidenav')--}}
-    @include('admin.layouts.partials.nav.menu-horizontal')
-</div>
--- resources/views/admin/layouts/partials/laraspace-notifs.blade.php
+++
@@ -1,9 +0,0 @@
-@if (Session::has('flash_notification.message'))
-    <div class="laraspace-notify hidden-xs-up" data-driver="{{config('laraspace.notification')}}" data-notify="{{ Session::get('flash_notification.level') }}" data-message="{{ Session::get('flash_notification.message') }}">
-    </div>
-@endif
-
-@if($errors->any())
-    <div class="laraspace-notify hidden-xs-up" data-driver="{{config('laraspace.notification')}}" data-notify="error" data-message="{{ $errors->first() }}">
-    </div>
-@endif
