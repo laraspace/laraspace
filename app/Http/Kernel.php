785e1d13a7078b7968725d763bfe424a34f08cf5
Laravel Shift <shift@laravelshift.com>
2017-09-15 06:09:00 +0000

    Shift Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Laraspace\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\TrustProxies::class,
         SettingsMiddleware::class,
     ];
 
