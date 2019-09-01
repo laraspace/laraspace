c96c3e2419c20436fe60eedf173e8167f32e8b1d
Marcos Rocha <marcosrocha85@gmail.com>
2019-09-01 16:50:15 -0300

    Changing to support both kind of flash_notification #16

--- resources/views/admin/layouts/partials/laraspace-notifs.blade.php
+++ resources/views/admin/layouts/partials/laraspace-notifs.blade.php
@@ -1,8 +1,13 @@
 @if (Session::has('flash_notification'))
-    @foreach(Session::get('flash_notification') as $flash_notification)
+    @if (Session::has('flash_notification.message'))
+    <div class="laraspace-notify hidden-xs-up" data-driver="{{config('laraspace.notification')}}" data-notify="{{ Session::get("flash_notification.level") }}" data-message="{{ Session::get("flash_notification.message") }}">
+    </div>
+    @else
+        @foreach(Session::get("flash_notification") as $flash_notification)
     <div class="laraspace-notify hidden-xs-up" data-driver="{{config('laraspace.notification')}}" data-notify="{{ $flash_notification->level }}" data-message="{{ $flash_notification->message }}">
     </div>
-    @endforeach
+        @endforeach
+    @endif
 @endif
 
 @if($errors->any())
